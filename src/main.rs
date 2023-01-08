4789e203c5d6b2721b0bd7fbfdfb896cec95e760
mohamedeliwa <mohamed.av11@gmail.com>
2023-01-09 00:29:38 +0200

    creating a function to convert a word to a pig latin

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+fn pig_latin(word: &str) -> String {
+    // this vector is built of type vec<&str> instead of vec<char> intentionally,
+    // to make it easier to check if it contains the first word letter or not,
+    // as on creating a slice out of the word it will be of type &str
+    let vowels = vec!["A", "a", "E", "e", "I", "i", "O", "o", "U", "u", "Y", "y"];
+    // converting the &str to String
+    // so string methods are accessible on our value
+    let mut word = word.to_string();
+
+    // if the word starts with a vowel
+    if vowels.contains(&&word[..1]) {
+        word.push_str("hay");
+
+    // if the word starts with consonant
+    } else {
+        word = format!("{}{}ay", &word[1..], &word[..1]);
+    };
+
+    return word;
+}
+
 fn main() {
-    println!("Hello, world!");
+    let word = pig_latin("word");
+
+    println!("{word}");
 }
